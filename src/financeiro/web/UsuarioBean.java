81f46d91cb9ea6750fed45df3314c463f6ab3f80
root <root@linux.(none)>
2014-02-21 11:30:46 -0400

    JDBC

--- src/financeiro/web/UsuarioBean.java
+++ src/financeiro/web/UsuarioBean.java
@@ -1,5 +1,10 @@
 package financeiro.web;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -26,8 +31,53 @@ public class UsuarioBean {
 		if(!this.senha.equalsIgnoreCase(this.confirmaSenha)){
 			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Senha confirmada incorretamente", ""));
 			return "usuario";
+		} 
+		try { 
+			Class.forName("org.postgresql.Driver"); 
+		} catch (ClassNotFoundException e) {
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "DATABASE Error 5364", ""));
+			return "usuario";
 		}
-		return "mostraUsuario";
+ 
+		Connection connection = null;
+ 
+		try { 
+			connection = DriverManager.getConnection(
+					"jdbc:postgresql://127.0.0.1:5432/book", "postgres",
+					"admin"); 
+		} catch (SQLException e) {
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "DATABASE Error 5364", ""));
+			return "usuario"; 
+		}
+ 
+		if (connection != null) {
+			PreparedStatement insert = null;
+			String sql = "INSERT INTO usuarios(nome, email, senha) VALUES(?,?,?)";
+			try {
+				insert = connection.prepareStatement(sql);
+				insert.setString(1, this.nome);
+				insert.setString(2, this.email);
+				insert.setString(3, this.senha);
+				insert.executeUpdate();
+			} catch (SQLException e) {
+				context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "DATABASE Error 5364", ""));
+				return "usuario"; 
+			} finally {
+				try {
+					insert.close();
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				
+			}
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Cadastrado com sucesso!", ""));
+			return "mostraUsuario";
+		} else {
+			context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "DATABASE Error 5364", ""));
+			return "usuario";
+		}
+		
 	}
 	
 	public void setNome(String nome) {
